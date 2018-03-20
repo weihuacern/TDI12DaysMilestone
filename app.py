0519f5d83f50d4cdd5e07186569e6e0276a8c542
weihuacern <hua.wei@email.ucr.edu>
2018-03-20 14:56:17 -0700

    update

--- app.py
+++ app.py
@@ -1,79 +1,88 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import pandas as pd
-from pandas import DataFrame,Series
-from datetime import datetime, timedelta
-import bokeh
-from bokeh.plotting import figure
+import datetime
+
 from bokeh.embed import components
-import os
-
-################################################################################
-# Username and password for any account (Quandl in this case, in private file)
-def loadApiKey( keyFile, keyName ) :
-	with open(keyFile) as f:
-		fromFile = {}
-		for line in f:
-			line = line.split() # to skip blank lines
-			if len(line)==3:
-				fromFile[line[0]] = line[2]			
-	f.close()
-	apiKey = fromFile[keyName]
-	return apiKey
-
-
-################################################################################
-# Fetch table from Quandl
-def fetchQuandl(ticker, apiKey) :
-
-  # Form the request URL
-	ticker = ticker.upper()
-
-	end = datetime.now().date()
-	start = end - timedelta(days=30)
-	
-  start_str = "&start_date=" + start.strftime("%Y-%m-%d")
-	end_str  = "&end_date=" + end.strftime("%Y-%m-%d")
-
-	reqUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + \
-					'.json?api_key=' + apiKey + start_str + end_str
-
-	print(reqUrl)
-  #End of URL generation
-
-  # HTTP request for dataframe and column name
-	r = requests.get(reqUrl)
-	if r.status_code < 400 :
-		# get name of company
-		name = r.json()['dataset']['name']
-		name = name.split('(')[0]
-
-		# get data
-		dat = r.json()['dataset']
-		df = DataFrame(dat['data'], columns=dat['column_names'] )
-		df = df.set_index(pd.DatetimeIndex(df['Date']))
-	else :
-		print("Stock ticker not valid")
-		df = None
-		name = None
-
-  return df, name
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.templates import RESOURCES
+from bokeh.util.string import encode_utf8
 
 app = Flask(__name__)
 
-# Initialization
-#app.vars = {}
-#keyFile = 'API_KEYS'
-#keyName = 'quandl'
-#app.vars['apiKey'] = loadApiKey( keyFile, keyName)
-
 @app.route('/')
-def index():
-  return render_template('index.html')
+def main():
+  return redirect(url_for('stock'))
 
 @app.route('/about')
 def about():
   return render_template('about.html')
 
+#Main Process
+@app.route('/stock/')
+def stock():
+  stockcode = request.args.get('stockcode')
+  stockperiod = str(request.args.get('period'))
+  
+  
+  if stockcode and stockcode.strip():
+    #request data from QuanDL
+    baseURL='https://www.quandl.com/api/v1/datasets/WIKI/'
+    periodURL={'1M':'?trim_start=' + (datetime.datetime.now() - datetime.timedelta(days=30)).strftime('%Y-%m-%d'), 
+      '6M':'?trim_start=' + (datetime.datetime.now() - datetime.timedelta(days=183)).strftime('%Y-%m-%d'), 
+      '1Y':'?trim_start=' + (datetime.datetime.now() - datetime.timedelta(days=365)).strftime('%Y-%m-%d'), 
+      'All':'', 
+      'None':''}
+    jsonURL=baseURL + stockcode + '.json' + periodURL[stockperiod]
+    jsonRespond = requests.get(jsonURL)
+    HTTPstatusCode=jsonRespond.status_code
+    
+    print '[URL]      ' + jsonURL
+    print '[HTTP]     ' + str(HTTPstatusCode)
+    print '[Stockcode]' + stockcode
+    print '[Period]   ' + stockperiod
+    
+    #parse data if HTTP Status is OK
+    if HTTPstatusCode==200:
+      jheader=(jsonRespond.json())['column_names']
+      jdata=(jsonRespond.json())['data']
+      stockdata=pd.DataFrame(jdata, columns=jheader)
+      stockdata["Date"] = pd.to_datetime(stockdata["Date"])
+      print stockdata.head()
+      
+      #To calculate the positions of the bars
+      mids = (stockdata.Open + stockdata.Close)/2
+      spans = abs(stockdata.Close-stockdata.Open)
+      #To check the up/down of the day to determin the bar color
+      inc = stockdata.Close > stockdata.Open
+      dec = stockdata.Open > stockdata.Close
+    
+      w = 12*60*60*1000 # half day in ms
+
+      #Render Chart
+      fig = figure(title=None, plot_width=600, plot_height=480,x_axis_type="datetime", toolbar_location="below", tools = "crosshair, pan,wheel_zoom,box_zoom,reset,resize")
+      
+      fig.segment(stockdata.Date, stockdata.High, stockdata.Date, stockdata.Low, color="black")
+      fig.rect(stockdata.Date[inc], mids[inc], w, spans[inc], fill_color="#D5E1DD", line_color="black")
+      fig.rect(stockdata.Date[dec], mids[dec], w, spans[dec], fill_color="#F2583E", line_color="black")
+
+      plot_resources = RESOURCES.render(
+        js_raw=INLINE.js_raw,
+        css_raw=INLINE.css_raw,
+        js_files=INLINE.js_files,
+        css_files=INLINE.css_files,
+      )
+
+      script, div = components(fig, INLINE)
+
+      html=render_template('stock.html', status={'code':1, 'msg':'OK'}, stock={'code':stockcode, 'period':stockperiod}, plot={'script':script, 'div':div, 'resources':plot_resources})
+    else:
+      html=render_template('stock.html', status={'code':2, 'msg':'Server Error'}, stock={'code':stockcode, 'period':stockperiod})
+  else:
+    html=render_template('stock.html', status={'code':3, 'msg':'Please Enter a Stockcode'}, stock={'code':'None', 'period':'None'})
+
+  return html 
+  
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(debug = True)
